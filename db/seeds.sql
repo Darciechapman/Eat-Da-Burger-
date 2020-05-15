defb09e0f01846e06221c2d87fb1f29ab2ffa49f
Darciechapman <darciechapman96@gmail.com>
2020-05-15 11:57:38 +0800

    inserting data into burgers_db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Beyond Burger");
+INSERT INTO burgers (burger_name) VALUES ("Hummus Falafel Burger");
+INSERT INTO burgers (burger_name) VALUES ("Garden Goodness Burger");
