4753cf2a427fc2e3d8209f33d61bfdfd52c23f72
Darciechapman <darciechapman96@gmail.com>
2020-05-15 22:22:04 +0800

    requiring connection

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const connection = require('../config/connection');
+const burgerJs = require('../models/burger');
+
+const router = express.Rounter();
+
+router.get("/", function(req, res) {
+    connection.query("SELECT * FROM burgers;", function(err, data) {
+        if (err) throw err;
+  
+        res.render("index", { burgers: data });
+    });
+});
+  
+app.post("/", function(req, res) {  
+    connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burger_name], function(err, result) {
+        if (err) throw err;
+  
+        res.redirect("/");
+    });
+});
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
