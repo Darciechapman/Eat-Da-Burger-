b1fe9403f44e0482f2ecd31e776b21b8587949a5
Darciechapman <darciechapman96@gmail.com>
2020-05-17 10:29:25 +0800

    linking orm and orm functions

--- server.js
+++ server.js
@@ -1,48 +1,7 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+const orm = require("./config/orm.js");
 
-var app = express();
+orm.selectAll("burger_name");
 
-var PORT = process.env.PORT || 8080;
+orm.insertOne("burger_name");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM burgers;", function(err, data) {
-    if (err) throw err;
-    res.render("index", { burgers: data });
-  });
-});
-
-app.post("/", function(req, res) {
-
-  connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burger_name], function(err, result) {
-    if (err) throw err;
-    res.redirect("/");
-  });
-});
-
-app.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+orm.updateOne("burger_name, devoured");
