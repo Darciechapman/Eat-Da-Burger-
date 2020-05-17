4e9b07de92b8bdba66c9f89888f7262b75b39a6d
Darciechapman <darciechapman96@gmail.com>
2020-05-17 10:22:54 +0800

    function selectAll, InsertOne, updateOne added

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,33 @@
 const connection = require('../config/connection.js');
 
-function selectAll() {
+let orm = {
+    selectAll: function(tableInput, colToSearch, valOfCol) {
+      var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+      connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
+    },
+    InsertOne: function(whatToSelect, table, orderCol) {
+      var queryString = "INSERT INTO ?? (??, ??) VALUES (?, ?);";
+      console.log(queryString);
+      connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
+    },
+    updateOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+      var queryString =
+        "UPDATE ?? SET ?? = ? WHERE ?? = ?;";
+  
+        connection.query(
+        queryString,
+        [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
+        function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    }
+};
 
-}
-
-function insertOne() {
-
-}
-
-function updateOne() {
-    
-}
+module.exports = orm;
