06c76ee853fc3a982ad1a1773efd9ce14da15750
Darciechapman <darciechapman96@gmail.com>
2020-05-18 13:18:17 +0800

    devoured = boolean defult false

--- config/orm.js
+++ config/orm.js
@@ -1,33 +1,29 @@
 const connection = require('../config/connection.js');
 
 let orm = {
-    selectAll: function(tableInput, colToSearch, valOfCol) {
-      var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-      connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-        if (err) throw err;
-        console.log(result);
-      });
-    },
-    InsertOne: function(whatToSelect, table, orderCol) {
-      var queryString = "INSERT INTO ?? (??, ??) VALUES (?, ?);";
-      console.log(queryString);
-      connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
-        if (err) throw err;
-        console.log(result);
-      });
-    },
-    updateOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
-      var queryString =
-        "UPDATE ?? SET ?? = ? WHERE ?? = ?;";
-  
-        connection.query(
-        queryString,
-        [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
-        function(err, result) {
-            if (err) throw err;
-            console.log(result);
-        });
-    }
+  selectAll: function(tableInput, colToSearch, valOfCol) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  InsertOne: function(whatToSelect, table, orderCol) {
+    var queryString = "INSERT INTO ?? (??, ??) VALUES (?, ?);";
+    console.log(queryString);
+    connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  updateOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+    var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?;";
+    connection.query(queryString, [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
+    function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  }
 };
 
 module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-	id INTEGER AUTO_INCREMENT NOT NULL,
-	burger_name VARCHAR(30) NOT NULL,
-    devoured INTEGER,
+	id INT AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
