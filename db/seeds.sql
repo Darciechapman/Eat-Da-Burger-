c9bb96a5d84693f10b2dadbc2478b85200efdb4e
Darciechapman <darciechapman96@gmail.com>
2020-05-18 13:27:03 +0800

    linking bootstrap and javascript

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+USE burgers_db;
+
 INSERT INTO burgers (burger_name) VALUES ("Beyond Burger");
 INSERT INTO burgers (burger_name) VALUES ("Hummus Falafel Burger");
 INSERT INTO burgers (burger_name) VALUES ("Garden Goodness Burger");
