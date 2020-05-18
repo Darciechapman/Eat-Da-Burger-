e48373a838b68e23d1e5b19c55cde288ed1428b5
Darciechapman <darciechapman96@gmail.com>
2020-05-18 14:20:50 +0800

    putting callback functions in burger varible

--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,38 @@
 const connection = require('../config/connection.js');
 
 function createQmarks(num) {
+
   var arr = [];
+
   for (var i = 0; i < num; i++) {
     arr.push("?");
   }
+
   return arr.toString();
 }
 
 function translateSql(ob) {
+
   var arr = [];
+
   for (var key in ob) {
     var value = ob[key];
+
     if (Object.hasOwnProperty.call(ob, key)) {
+
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
+
       arr.push(key + "=" + value);
     }
   }
+
   return arr.toString();
 }
 
 let orm = {
+
   selectAll: function(table, cb) {
 
     let queryString = `SELECT * FROM ${table};`;
--- models/burgers.js
+++ models/burgers.js
@@ -1,7 +1,29 @@
 const orm = require("./config/orm.js");
 
-orm.selectAll("burger_name");
-
-orm.insertOne("burger_name");
-
-orm.updateOne("burger_name, devoured");
+let burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+  
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+  
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+  
+    deleteOne: function(condition, cb) {
+        orm.deleteOne("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+  
+module.exports = burger;
