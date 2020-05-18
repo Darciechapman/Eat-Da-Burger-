e610f947497a0e3ad828830a745ada074b7b9b6a
Darciechapman <darciechapman96@gmail.com>
2020-05-18 13:53:49 +0800

    linking controllers/routes

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./contollers/burgers_controller.js");
+app.use(routes);
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
